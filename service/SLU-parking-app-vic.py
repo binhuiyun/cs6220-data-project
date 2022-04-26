0800c28d2d758a14fdb1aff8e149cb7b10928314
Jarvis Ren <jarvisren@Jarviss-MacBook-Pro.local>
2022-04-25 18:54:57 -0700

    add parking spot

--- service/SLU-parking-app-vic.py
+++ service/SLU-parking-app-vic.py
@@ -6,7 +6,7 @@ import pandas as pd
 from streamlit_folium import folium_static
 import folium
 from folium import plugins
-
+import math
 
 def user_input_date():
     date = st.sidebar.date_input(
@@ -22,8 +22,26 @@ def user_input_date():
     return (day_of_the_week, minute_of_the_day)
 
 def user_confirm():
-    res = st.sidebar.button("See result")
+    res = st.sidebar.button("See Result")
     return res
+
+def user_input_street(data):
+    blocks = data['BlockfaceName'].tolist()
+    blocks = sorted(blocks)
+
+    location_selector = st.sidebar.selectbox(
+        "Select a location",
+        blocks
+    )
+
+    lat = data[(data['BlockfaceName'] == location_selector)]['Latitude']
+
+    lon = data[ (data['BlockfaceName'] == location_selector)]['Longitude']
+    
+    lat = lat.values[0]
+    lon = lon.values[0]
+
+    return lat, lon
 
 def generate_model_input_map(day, minute, coordinates):
     num_coor = len(coordinates)
@@ -44,29 +62,37 @@ def predict_map(model, day, minute, coordinates):
     points = new_df.to_numpy()
     return points
 
-def generate_model_input_point(day, minute, Latitude, Longitude):
+
+def predict_street(model, day, minute, Latitude, Longitude):
     col = ['DayOfTheWeek', 'MinuteOfTheDay', 'Latitude', 'Longitude']
     user_in = [day, minute, Latitude, Longitude]
     user_df = pd.DataFrame(user_in).T
     user_df.columns = col
-    return user_df
-
-def map(array):
-    m = folium.Map(location=[47.6256, -122.3344], zoom_start=14)
-    if any(array):
-        plugins.HeatMap(array).add_to(m)
-    folium_static(m)
+    res = model.predict(user_df)
+    return res
 
 
 # Main Panel
 model = joblib.load('../parking/random-forest.joblib')
 coordinates = pd.read_csv('../parking/data/paystub_coordinates.csv')
+paystubs = pd.read_csv('../parking/data/paystub_location.csv')
+
 day, minute = user_input_date()
 
+lat, lon = user_input_street(paystubs)
+
 m = folium.Map(location=[47.6256, -122.3344], zoom_start=15)
+
 if user_confirm():
     predict_map(model, day, minute, coordinates)
-
     points = predict_map(model, day, minute, coordinates)
     plugins.HeatMap(points).add_to(m)
+
+    num = predict_street(model, day, minute, lat, lon)
+    if num[0] > 0:
+        st.write(math.floor(num[0]), "parking space is available on your selected street!")
+    else:
+        st.write('No available parking on your selected street!')
+
 folium_static(m)
+

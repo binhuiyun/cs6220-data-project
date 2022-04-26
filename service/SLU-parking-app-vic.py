406f25bba0695bd23f586e5ea927509709578486
Jarvis Ren <jarvisren@Jarviss-MacBook-Pro.local>
2022-04-25 17:05:24 -0700

    add folim map

--- service/SLU-parking-app-vic.py
+++ service/SLU-parking-app-vic.py
@@ -2,27 +2,56 @@ import streamlit as st
 import joblib
 import datetime
 import pandas as pd
+from streamlit_folium import folium_static
+import folium
+from folium import plugins
 
 # model = joblib.load('../parking/random-forest.joblib')
 
-def user_input_features():
+def user_input_date():
     date = st.sidebar.date_input(
         "Select a date",
         datetime.date(2022, 3, 22)
     )
-    hour = st.sidebar.slider('Hour', 8, 18, 14)
-    minute= st.sidebar.slider('Minute',0, 60, 1)
+    hour = st.sidebar.slider('Hour', 8, 18, 0)
+    minute= st.sidebar.slider('Minute',0, 60, 0)
 
-    day_of_the_week = date.day_of_week
+    day_of_the_week = date.weekday
     minute_of_the_day = hour * 60 + minute
 
     return (day_of_the_week, minute_of_the_day)
 
 
-def generate_map_weight(day, minute, coordinates):
-    coordinates
+def generate_model_input_map(day, minute, coordinates):
+    num_coor = len(coordinates)
+    day_list, minute_list = [], []
+    day_list += num_coor * [day]
+    minute_list += num_coor * [minute]
+    coordinates['DayOfTheWeek'] = day_list
+    coordinates['MinuteOfTheDay'] = minute_list
+    return coordinates
 
-def main():
-    coordinates = pd.read_csv('../parking/data/paystub_coordinates.csv')
-    print(len(coordinates))
-main()
+def generate_model_input_point(day, minute, Latitude, Longitude):
+    col = ['DayOfTheWeek', 'MinuteOfTheDay', 'Latitude', 'Longitude']
+    user_in = [day, minute, Latitude, Longitude]
+    user_df = pd.DataFrame(user_in).T
+    user_df.columns = col
+    return user_df
+
+def map(array):
+    m = folium.Map(location=[47.6256, -122.3344], zoom_start=14)
+    if any(array):
+        plugins.HeatMap(array).add_to(m)
+    folium_static(m)
+
+# Main Panel
+# coordinates = pd.read_csv('../parking/data/paystub_coordinates.csv')
+user_input_date()
+map([])
+
+# def main():
+#     coordinates = pd.read_csv('../parking/data/paystub_coordinates.csv')
+#     test = generate_model_input_map(10, 100, coordinates)
+#     print(test)
+
+# main()
